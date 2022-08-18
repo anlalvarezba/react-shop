52b938a3c539ee5a819e2f3d85ceb250437492ff
anlalvarezba <anlalvarezba@unal.edu.co>
2022-08-18 18:56:25 -0500

    Skeleton to work in the practical React course.

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+    return (
+        <h1>Hola mundo, esta vaina funciono...</h1>
+    );
+};
+
+export default App; 
