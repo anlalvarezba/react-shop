44c50b560bd27eddf3457f44ea5cc4efc44bb1c0
anlalvarezba <anlalvarezba@unal.edu.co>
2022-08-24 12:25:32 -0500

    Files were arranged into components, containers and routes. Their styles were added too. Atomic design was used to it.

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import { Layout } from '../containers/Layout';
-import { Login } from '../containers/Login';
 import { Home } from '../pages/Home';
+import { Login } from '../pages/Login';
+import { PasswordRecovery } from '../pages/PasswordRecovery';
+import { SendEmail } from '../pages/SendEmail';
+import { NewPassword } from '../pages/NewPassword';
+import { MyAccount } from '../pages/MyAccount';
+import { CreateAccount } from '../pages/CreateAccount';
+import { Checkout } from '../pages/Checkout';
+import { Orders } from '../pages/Orders';
 import { NotFound } from '../pages/NotFound';
-import { RecoveryPassword } from '../containers/RecoveryPassword';
 import '../styles/global.css';
+
 
 const App = () => {
     return (
@@ -15,7 +22,15 @@ const App = () => {
                 <Routes>
                     <Route path='/' element={<Home/>} />
                     <Route path='/login' element={<Login/>} />
-                    <Route path='/recovery-password' element={<RecoveryPassword/>} />
+                    <Route path='/password-recovery' element={<PasswordRecovery/>} />
+                    
+                    <Route path='/send-email' element={<SendEmail/>} />
+                    <Route path='/new-password' element={<NewPassword/>} />
+                    
+                    <Route path='/account' element={<MyAccount />} />
+                    <Route path='/signup' element={<CreateAccount />} />
+                    <Route path='/checkout' element={<Checkout/>} />
+                    <Route path='/orders' element={<Orders/>} />
                     <Route path='*' element={<NotFound />} />
                 </Routes>
             </Layout>
